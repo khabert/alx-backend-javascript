247b47e8dec0c7081a0c7a6c01942db8e42a0300
Khabert <khabertzion11@gmail.com>
2023-10-03 16:34:14 +0000

    has array value n

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,10 +1,3 @@
-i/**
- * Checks if a set contains each element in an array.
- * @param {Set} set - The collection of unique items.
- * @param {*} array - The array of items.
- * @author kabagambe herbert <https://github.com/Khabert>
- * @returns {Boolean}
- */
-const hasValuesFromArray = (set, array) => array.every((value) => set.has(value));
-
-export default hasValuesFromArray;
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
